e16694d2a0fd59533f7d48f245b7cd06365c4e62
Gustavokvs <gustavokruger514@gmail.com>
2024-09-19 22:06:13 -0300

    Foi dado um site pra gente entender

---
+++ AulaJavaScript3/numeroAlgunsCuidados.js
@@ -0,0 +1,9 @@
+console.log(7 / 0.0);
+console.log(7 / 0.00001);
+console.log("10" / 2); // Faz a divisão pois o simbulo / não faz sentido neste contexto.
+console.log('3' + 2); // Faz a concatenação que gera 32 pois essa é uma função do simbulo +.
+console.log('3' - 2); // Faz a subtração pois o simbulo - não faz sentido neste contexto.
+console.log("Show" * 2);
+console.log(0.1 + 0.7);
+// console.log(10.toString());
+console.log((10.345).toFixed(2));
