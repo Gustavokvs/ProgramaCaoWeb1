a6d7b7f071bee237b6ec4913e74df02027bb99b0
Gustavokvs <gustavokruger514@gmail.com>
2024-09-23 22:02:37 -0300

    Aula de algumas functions

---
+++ AulaJavaScript4/funcao1/funcaohello.js
@@ -0,0 +1,8 @@
+function Inicio() {
+    var msg = '';
+    msg = "Olá mundo";
+    console.log(msg);
+    msg = "Me livrei da maldição";
+    console.log(msg);
+}
+Inicio();
---
+++ AulaJavaScript4/funcao2/funcaoValores.js
@@ -0,0 +1,16 @@
+function Valores(N1, N2) {
+    // Função :
+    // Autor :
+    // Data : 25/07/2017
+    // Seção de Declarações 
+
+    let M = 0;
+
+
+    M = (N1 + N2) / 2
+    console.log("A Média entre " + N1 + " e " + N2 + " é " + M)
+    console.log(M)
+    // Seção de Comando
+
+}
+Valores(2, 6);
